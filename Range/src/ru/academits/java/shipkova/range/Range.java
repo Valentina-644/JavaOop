7af39b53e8bdf97b0750709477254cdf379f077f
Valentina-644 <72196216+Valentina-644@users.noreply.github.com>
2020-10-02 15:21:27 +0700

    Update Range.java

--- Range/src/ru/academits/java/shipkova/range/Range.java
+++ Range/src/ru/academits/java/shipkova/range/Range.java
@@ -2,7 +2,7 @@ package ru.academits.java.shipkova.range;
 
 public class Range {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 
 
